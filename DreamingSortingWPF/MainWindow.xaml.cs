44084295ddd73e166312a5e11591d6a20caf895c
DreamingCodes <lore.rizzotti@gmail.com>
2021-10-26 21:28:07 +0200

    Initial commit

---
+++ DreamingSortingWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DreamingSortingWPF {
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                DragMove();
+            }
+        }
+        void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        
+        void Nick_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                System.Diagnostics.Process.Start("https://dreaming.codes");
+            }
+        }
+    }
+}
